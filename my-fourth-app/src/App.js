09969d15c5c2910f96e2576d46b9f5766fafbf4b
Huy Nguyen <quanghuy871@gmail.com>
2021-06-27 12:12:41 +0700

    Update

--- my-fourth-app/src/App.js
+++ my-fourth-app/src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
@@ -6,6 +6,11 @@ import MainHeader from './components/MainHeader/MainHeader';
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const userInformation = localStorage.getItem('isLoggedIn');
+    if (userInformation === '1') setIsLoggedIn(true);
+  }, []);
 
   const loginHandler = (email, password) => {
     // We should of course check email and password
