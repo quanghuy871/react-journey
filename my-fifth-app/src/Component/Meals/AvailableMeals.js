bf8fe2b13b6306021c1f83642dc367f598522dbe
Huy Nguyen <quanghuy871@gmail.com>
2021-08-10 00:08:52 +0700

    New

--- my-fifth-app/src/Component/Meals/AvailableMeals.js
+++ my-fifth-app/src/Component/Meals/AvailableMeals.js
@@ -4,15 +4,19 @@ import Card from '../UI/Card';
 import MealItem from './MealItems/MealItem';
 
 const AvailableMeals = (props) => {
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
   const [meals, setMeals] = useState([]);
+  const [error, setError] = useState('');
 
   useEffect(() => {
     const fetchMeals = async () => {
-      setLoading(true);
       const res = await fetch('https://meals-e7310-default-rtdb.asia-southeast1.firebasedatabase.app/meals.json');
       const data = await res.json();
       const mealsArr = [];
+
+      if (!res.ok) {
+        throw new Error();
+      }
 
       for (const key in data) {
         mealsArr.push({
@@ -26,15 +30,20 @@ const AvailableMeals = (props) => {
       setMeals(mealsArr);
       setLoading(false);
     };
-    fetchMeals();
+
+    fetchMeals().catch((e) => {
+      setLoading(false);
+      setError(e.message);
+    });
   });
 
   const mealList = meals.map(el => <MealItem key={el.id} id={el.id} name={el.name} description={el.description} price={el.price}/>);
   return (
     <section className={classes.meals}>
       <Card>
-        {!loading && <p>Loading...</p>}
-        {loading && <ul>{mealList}</ul>}
+        {loading && <p>Loading...</p>}
+        {!loading && <ul>{mealList}</ul>}
+        {error}
       </Card>
     </section>
   );
