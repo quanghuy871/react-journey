e829903e76bdc6f054f2bd5c18c7a7006a14d232
Huy Nguyen <quanghuy871@gmail.com>
2021-08-21 23:54:49 +0700

    Update

--- my-eighth-app/src/store/index.js
+++ my-eighth-app/src/store/index.js
@@ -23,7 +23,6 @@ const counterSlice = createSlice({
 const authSlice = createSlice({
   name: 'authenticator',
   initialState: authenticationState,
-
   reducers: {
     login(state) {
       state.authentication = true;
